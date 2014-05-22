6436f209d71b173376cbdff2624d6921c507afc1
tina98 <tinali3698@gmail.com>
2014-05-22 15:01:39 -0400

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -1,4 +1,10 @@
+var score=1;
 
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+function UpScore(){
+    document.getElementById("score").innerHTML=score;
+    score++;
 }
