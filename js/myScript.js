a2a31c84fa1b4af330a71558e75d029ed4b9cf85
tina98 <tinali3698@gmail.com>
2014-05-24 23:16:54 -0400

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -15,30 +15,30 @@ function UpScore(){
 //     $("#beginning").hide();
 //   });
 
-function ShowQuestion(){
-    document.getElementbyId("#question#").innerHTML=qnum;
-}
-
-function UpQuestion(){
-    qnum++;
-    currentquestion[qnum];
-    ShowQuestion();
-}
-
-
-function HideInstructions(){
-    $("#beginnning").fadeOut();
-}
-
-// function CrossOut(var wrong){
-//     document.getElementById().style.text-decoration="line-through";
+// function ShowQuestion(){
+//     document.getElementbyId("#question#").innerHTML=qnum;
 // }
 
-function InitalizeQuestion(){
-    document.getElementById("#mc1").innerHTML="sdfsdf";
-}
+// function UpQuestion(){
+//     qnum++;
+//     currentquestion[qnum];
+//     ShowQuestion();
+// }
 
-currentquestion[1]=001;
-currentquestion[2]=002;
 
-var 001=[];
+// function HideInstructions(){
+//     $("#beginnning").fadeOut();
+// }
+
+// // function CrossOut(var wrong){
+// //     document.getElementById().style.text-decoration="line-through";
+// // }
+
+// function InitalizeQuestion(){
+//     document.getElementById("#mc1").innerHTML="sdfsdf";
+// }
+
+// currentquestion[1]=001;
+// currentquestion[2]=002;
+
+// var 001=[];
